7c1e12d796153e6862db034bf10b615e1f9f680b
cellle <91531988+celinerh@users.noreply.github.com>
2023-02-14 13:20:41 +0100

    implement class hook

---
+++ src/hooks/useClass.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const useClass = () => {
+  const [gymClass, setGymClass] = useState();
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/api/v1/classes/${id}`, {
+      method: "GET",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setGymClass(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setIsPending(false);
+        setError(error.message);
+      });
+  }, []);
+
+  return { gymClass, error, isPending };
+};
+
+export default useClass;
