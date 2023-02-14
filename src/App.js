06d134e47dca67fe2c84aa984ee2bcc6b304bec0
cellle <91531988+celinerh@users.noreply.github.com>
2023-02-14 13:20:05 +0100

    remove padding from app container

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App h-screen p-6">
+    <div className="App h-screen">
       <Outlet />
     </div>
   );
