27f4f3784cf7905f34da271922481e63f90841bb
cellle <91531988+celinerh@users.noreply.github.com>
2023-02-15 13:03:05 +0100

    schedule

--- src/pages/MySchedule.js
+++ src/pages/MySchedule.js
@@ -1,5 +1,12 @@
+import Navigation from "../components/Navigation";
+
 function MySchedule() {
-  return <div>MySchedule</div>;
+  return (
+    <div className="p-6">
+      <Navigation goBack />
+      <h1 className="heading1">My Schedule</h1>
+    </div>
+  );
 }
 
 export default MySchedule;
