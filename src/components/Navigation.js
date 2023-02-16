501e3fb3e02a76c920d62bc8db64d7f4372497b0
cellle <91531988+celinerh@users.noreply.github.com>
2023-02-16 11:12:35 +0100

    support style on component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,13 @@ import { HiMenuAlt3 } from "react-icons/hi";
 import GoBack from "./GoBack";
 import Menu from "./Menu";
 
-function Navigation({ goBack, title }) {
+function Navigation({ goBack, title, className }) {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <>
       {showMenu && <Menu setShowMenu={setShowMenu} />}
-      <header className="mb-4">
+      <header className={`mb-4 ${className}`}>
         <nav>
           <ul className="flex items-center justify-between">
             <li>{goBack && <GoBack />}</li>
